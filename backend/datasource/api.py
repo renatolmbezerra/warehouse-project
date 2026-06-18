36914189328fe300d1028778c11aa192f412d079
Renato-Tecpel <renatolmb@hotmail.com>
2026-06-18 20:16:26 -0300

    api finalizada

---
+++ backend/datasource/api.py
@@ -0,0 +1,14 @@
+
+
+class APICollector:
+    def __init__(self):
+            return
+    
+    def start(self):
+          
+
+    def getData(self):
+          
+
+    def extractData(self):
+          return
