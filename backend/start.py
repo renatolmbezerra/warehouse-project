853c8ae2977fceaf10d75220b08d781964655e0d
Renato Macedo <renatolmb@hotmail.com>
2026-06-25 06:39:25 -0300

    feat: schedule criado para rodar a extracao de forma periodica

--- backend/start.py
+++ backend/start.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from contracts.schema import CompraShema
 from tools.aws.client import S3Client
 
+import time
 import schedule
 
 # Carrega as variáveis do arquivo .env para o sistema
@@ -11,6 +12,15 @@ load_dotenv()
 schema = CompraShema
 aws = S3Client()
 
-minha_classe = APICollector(schema, aws).start(5)
+def apiCollector(schema, aws, repeat):
+    response = APICollector(schema, aws).start(repeat)
+    print('Executei')
+    return response
 
-print(minha_classe)
+schedule.every(1).minutes.do(apiCollector, schema, aws, 50)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
