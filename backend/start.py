a5eefe2970d4c3f7414c695ba47c16b4bef41abd
Renato Macedo <renatolmb@hotmail.com>
2026-06-26 21:26:10 -0300

    feat: ajustes no arquivo start.py

--- backend/start.py
+++ backend/start.py
@@ -1,6 +1,6 @@
 from datasource.api import APICollector
 from dotenv import load_dotenv
-from contracts.schema import CompraShema
+from contracts.schema import CompraSchema
 from tools.aws.client import S3Client
 
 import time
@@ -9,15 +9,23 @@ import schedule
 # Carrega as variáveis do arquivo .env para o sistema
 load_dotenv()
 
-schema = CompraShema
+schema = CompraSchema
 aws = S3Client()
 
 def apiCollector(schema, aws, repeat):
     response = APICollector(schema, aws).start(repeat)
-    print('Executei')
+
+    if response:
+        print(f"[{time.strftime('%H:%M:%S')}] Executei com sucesso: Arquivo salvo no S3.")
+    else:
+        print(f"[{time.strftime('%H:%M:%S')}] Executei, mas nenhum dado foi salvo (vazio ou erro).")
+
     return response
 
+# Agenda a execução a cada 1 minuto passando o parâmetro 50
 schedule.every(1).minutes.do(apiCollector, schema, aws, 50)
+
+print("Iniciando o agendador. Pressione Ctrl+C para sair.")
 
 while True:
     schedule.run_pending()
