8333b491bcb19ed146251e05859c93bd4758034f
Renato-Tecpel <renatolmb@hotmail.com>
2026-07-16 18:27:01 -0300

    feat: implementando dbt-duckdb para processamento e materializacao da silver no s3

---
+++ dbt_warehouse/models/silver/slv_tecpel_titmmov.sql
@@ -0,0 +1,19 @@
+{{ config(
+    location="s3://" ~ env_var('S3_BUCKET_NAME') ~ "/silver/sqlserver/Tecpel/TITMMOV/slv_tecpel_titmmov.parquet"
+) }}
+
+WITH source AS (
+    SELECT *
+    FROM {{ source('bronze_tecpel', 'titmmov') }}
+),
+
+deduplicated AS (
+    SELECT *
+    FROM source
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY CODCOLIGADA, IDMOV, NSEQITMMOV 
+        ORDER BY dt_extracao DESC
+    ) = 1
+)
+
+SELECT * FROM deduplicated
