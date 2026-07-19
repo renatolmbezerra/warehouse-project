bdce9db3c0ed179ce5608cec49eaef9e4e6b693b
Renato-Tecpel <renatolmb@hotmail.com>
2026-07-18 23:23:30 -0300

    feat: finalizando a implementacao de um data lakehouse com iceberg no s3

--- dbt_warehouse/models/silver/slv_tecpel_titmmov.sql
+++ dbt_warehouse/models/silver/slv_tecpel_titmmov.sql
@@ -1,10 +1,19 @@
 {{ config(
-    location="s3://" ~ env_var('S3_BUCKET_NAME') ~ "/silver/sqlserver/Tecpel/TITMMOV/slv_tecpel_titmmov.parquet"
+    materialized='incremental',
+    incremental_strategy='merge',
+    format='iceberg',
+    unique_key=['CODCOLIGADA', 'IDMOV', 'NSEQITMMOV'],
+    location="s3://" ~ env_var('S3_BUCKET_NAME') ~ "/silver/sqlserver/Tecpel/TITMMOV"
 ) }}
 
 WITH source AS (
     SELECT *
     FROM {{ source('bronze_tecpel', 'titmmov') }}
+    
+    {% if is_incremental() %}
+    -- Lê apenas os dados da Bronze extraídos após a última carga desta tabela Silver
+    WHERE dt_extracao > (SELECT MAX(dt_extracao) FROM {{ this }})
+    {% endif %}
 ),
 
 deduplicated AS (
--- dbt_warehouse/models/silver/slv_tecpel_tmov.sql
+++ dbt_warehouse/models/silver/slv_tecpel_tmov.sql
@@ -1,10 +1,19 @@
 {{ config(
-    location="s3://" ~ env_var('S3_BUCKET_NAME') ~ "/silver/sqlserver/Tecpel/TMOV/slv_tecpel_tmov.parquet"
+    materialized='incremental',
+    incremental_strategy='merge',
+    format='iceberg',
+    unique_key=['CODCOLIGADA', 'IDMOV'],
+    location="s3://" ~ env_var('S3_BUCKET_NAME') ~ "/silver/sqlserver/Tecpel/TMOV"
 ) }}
 
 WITH source AS (
     SELECT *
     FROM {{ source('bronze_tecpel', 'tmov') }}
+    
+    {% if is_incremental() %}
+    -- Lê apenas os dados da Bronze extraídos após a última carga desta tabela Silver
+    WHERE dt_extracao > (SELECT MAX(dt_extracao) FROM {{ this }})
+    {% endif %}
 ),
 
 deduplicated AS (
